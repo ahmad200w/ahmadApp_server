990b8503c47bd45e3d1b5f67653e1c3515d86bb3
ahmad200w <ahmadka94@hotmail.com>
2024-03-02 18:46:28 +0100

    start

--- Controllers/useController.js
+++ Controllers/useController.js
@@ -103,9 +103,9 @@ const findeAllUser =async (req, res)=>{
  
 }
 const sendOrder = async (req, res) => {
-  const { userName, email, password, orders, total } = req.body;
+  const { userName, email,  orders, total } = req.body;
 
-  if (!userName || !email  ) {
+  if (!userName || !email) {
     return res.status(407).json({ message: "Username, email, and password are required" });
   }
 
@@ -119,12 +119,7 @@ const sendOrder = async (req, res) => {
       return res.status(401).json({ message: "User not found" });
     }
 
-    const match = await comparePassword(password, user.password);
-
-    if (!match) {
-      return res.status(401).json({ message: "Invalid password" });
-    }
-    console.log(match)
+   
   
     // إضافة طلبات والمجموع للمستخدم
     user.orders = orders;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const app = require("./App")
-const port = process.env.port || 7500 ;
+const port = process.env.port || 6500 ;
 const server = http.createServer(app);
 module.exports = server;
 app.listen(port);
